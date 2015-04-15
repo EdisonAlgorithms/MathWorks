0c30c71ab82e96bceddbe103543f2142df9d4c77
zeyuanxy <zeyuanxy@gmail.com>
2015-04-15 13:59:45 +0800

    add Problem 1. Times 2 - START HERE

---
+++ vol1/1.m
@@ -0,0 +1,9 @@
+function y = times2(x)
+
+% Modify the line below so that the output y is twice the incoming value x
+
+y = 2 *  x;
+
+% After you fix the code, press the "Submit" button, and you're on your way.
+
+end
