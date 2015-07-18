7b9cad730e74a4c5c8a6b8b5a533b19913ae12de
zeyuanxy <zeyuanxy@gmail.com>
2015-07-19 01:10:55 +0800

    add 13

--- vol1/013.m
+++ vol1/013.m
@@ -1,6 +1,6 @@
 function s2 = refcn(s1)
     s2 = [];
-    s1 = regexp(s1, '[^aeiouAEIOU]', 'match');
+    s1 = regexp(s1, '[aeiouAEIOU0-9_\W]', 'match');
     for i = 1:length(s1)
         s2 = [s2 s1{i}]
     end
